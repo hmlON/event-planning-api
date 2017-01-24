2e2bbeae7e82f91ed8cc58c505d6245bc784dee7
Nikita Kholin <nik.kholin@gmail.com>
2017-01-24 23:48:53 +0200

    Add request spec generation

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,8 @@ module EventPlanningApi
                        view_specs: false,
                        helper_specs: false,
                        routing_specs: false,
-                       controller_specs: true,
-                       request_specs: false
+                       controller_specs: false,
+                       request_specs: true
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
   end
