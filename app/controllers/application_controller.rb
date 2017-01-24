d647d994eb897f1a32fbf76c0f58f3eb306d769b
Nikita Kholin <nik.kholin@gmail.com>
2017-01-25 01:04:26 +0200

    Add current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def current_user
+    @current_user ||= User.find(doorkeeper_token[:resource_owner_id])
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
