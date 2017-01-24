043ed1015fdb95b4e00d7459658d423887e7ec22
Nikita Kholin <nik.kholin@gmail.com>
2017-01-24 17:48:35 +0200

    Add event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :user
+  validates_presence_of :user, :time, :place, :description
+  validates_length_of :place, { in: (5..50) }
+  validate :time_cannot_be_in_the_past
+
+  private
+
+  def time_cannot_be_in_the_past
+    errors.add(:time, "can't be in the past") if time < Time.current
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,3 +1,18 @@
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:event)).to be_valid
+  end
+
+  it 'is invalid without user' do
+    expect(build(:event, user: nil)).not_to be_valid
+  end
+
+  it 'is invalid with time in the past' do
+    expect(build(:event, time: Time.current + 1.second)).to be_valid
+    expect(build(:event, time: Time.current - 1.second)).not_to be_valid
+  end
+
+  it 'is invalid with place length more then 50' do
+    expect(build(:event, place: "a"*51)).not_to be_valid
+  end
 end
